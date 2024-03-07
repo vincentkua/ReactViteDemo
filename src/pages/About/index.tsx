19d71cc2a7bb833cef95afe064be801cfac20b50
vincentkua <zylyn89@gmail.com>
2024-03-07 22:52:05 +0800

    init: initiate for react typescript notes

---
+++ src/pages/About/index.tsx
@@ -0,0 +1,15 @@
+const About = () => {
+  return (
+    <>
+      <h3>About</h3>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Debitis
+        corporis harum, beatae ducimus velit sapiente, nihil facilis cumque
+        ipsam dignissimos impedit sit numquam quibusdam eveniet eius repudiandae
+        voluptas? Culpa, aliquid.
+      </p>
+    </>
+  );
+};
+
+export default About;
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,15 @@
+const Home = () => {
+  return (
+    <>
+      <h3>Home Page</h3>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus nihil,
+        laudantium minus eveniet iusto illum aperiam pariatur velit distinctio
+        ipsam ut hic odio reiciendis dolor exercitationem ea accusantium
+        provident? Excepturi?
+      </p>
+    </>
+  );
+};
+
+export default Home;
